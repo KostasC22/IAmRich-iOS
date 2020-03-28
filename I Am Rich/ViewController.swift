e332393ac773e407de224fc207f06d08ce7c12fb
Kostas Chaviaras <chaviarask02@gmail.com>
2020-03-28 12:27:37 +0200

    initial commit of the project.

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by kostas on 26/3/20.
+//  Copyright © 2020 HaviStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
